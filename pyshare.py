14aab34d943ec6916f112bfbecfe6e3132dd98f0
plutphil <47215043+plutphil@users.noreply.github.com>
2021-06-23 23:29:28 +0200

    Add files via upload
    -fixed 
    -TODO: download progress not shown in firefox

--- pyshare.py
+++ pyshare.py
@@ -4,6 +4,8 @@ import os
 import socket
 import sys
 localhostname=socket.gethostbyname(socket.gethostname())
+print(localhostname)
+print(socket.gethostbyaddr(localhostname))
 print ('Number of arguments:', len(sys.argv), 'arguments.')
 print ('Argument List:', str(sys.argv))
 filename=str(sys.argv[1])
@@ -11,9 +13,12 @@ port = 4443
 from base64 import b64encode
 import uuid
 randomuuid= str(uuid.uuid4().hex)
+if(len(sys.argv)==3):
+    if(sys.argv[2]=="-p"):
+        randomuuid="pub"
 print ("https://"+localhostname+":"+str(port)+"/"+randomuuid)
 os.system("openssl req -subj '/O=PyShare/C=US/CN="+localhostname.replace(".","-")+"' -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -keyout key.pem -out cert.pem")
-print(localhostname)
+
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         print(self.path)
@@ -24,10 +29,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-Disposition', 'attachment; filename="'+ os.path.basename(filename)+'"')
                 self.end_headers()
                 blocksize=4096
-                data = file.read(blocksize)
-                while(len(data)==blocksize):
+                while True:
                     data = file.read(blocksize)
                     self.wfile.write(data)
+                    if(len(data)!=blocksize):
+                        break
                 return
         self.send_response(200)
         self.end_headers()
